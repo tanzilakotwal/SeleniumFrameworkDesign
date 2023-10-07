4d4855dd40e87919eece71925fc33115fa256214
TanzilaKotwal <tanzilakotwal22@gmail.com>
2023-10-07 17:34:00 +0530

    updated in secondary

--- src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
+++ src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 //this is abstract component class . 
 //updated in master 
+//updated in cloned version 
 public class AbstractComponent {
 	
 	WebDriver driver ;
